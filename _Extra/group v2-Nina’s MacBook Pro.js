b83ac565ad1e0000557ca43d49c42e7496372e0c
Nik <nik.piao26@gmail.com>
2024-05-16 14:59:12 -0400

    added multiple files and folders for this group project

---
+++ _Extra/group v2-Nina’s MacBook Pro.js
@@ -0,0 +1,59 @@
+// Nik
+
+
+// Function to open the overlay navigation 
+// left to right (0 to 100% width), when it is triggered:
+
+function openNav() {
+    document.querySelector('.navbar').style.width = '100%';
+}
+
+// Function to close the overlay navigation
+function closeNav() {
+    document.querySelector('.navbar').style.width = '0';
+}
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+    console.log('I was clicked');
+});
+
+
+const insta = document.querySelector('.fa-instagram');
+
+// NINA JAVASCRIPT STUFF
+
+// const promoCard1 = document.querySelector("#nina1");
+
+// promoCard1.addEventListener("click", (e) => {
+//     console.log("nina");
+// })
+
+const inputField = document.querySelector("#searchInput");
+
+console.log(inputField.value);
+
+const whereToButton = document.querySelector(".button");
+let searchUrl = "https://www.google.com/search?q=" + inputField;
+
+whereToButton.addEventListener("click", (e) => {
+        window.location.href = searchUrl;
+    
+});
+
+const losAngelesCTA = document.querySelector(".CTA1");
+
+losAngelesCTA.addEventListener("click", () => {
+    window.location.href = "https://www.discoverlosangeles.com/";
+});
+
+
+// localStorage
+//     => option + cmd + j to open console;
+ //     => find >>; then after look for application(7th item);
+//     => select localStorage, inside is the website
+//     => key and value pair
+    
+
+
